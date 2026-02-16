5e6405377940d683a277c266a2ba425a9e56f1db
ybrain <ybrain@DESKTOP-3EE7OHH.localdomain>
2026-02-16 19:09:24 +0900

    Branch테스트해봄(USB_CDC)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -13,6 +13,7 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
+  *This code is meant to test the USB CDC connection!!
   ******************************************************************************
   */
 /* USER CODE END Header */
