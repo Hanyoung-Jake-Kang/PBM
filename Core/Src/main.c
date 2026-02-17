524e2432ec68660cb81266092110696110f2e4c9
ybrain <ybrain@DESKTOP-3EE7OHH.localdomain>
2026-02-17 09:54:58 +0900

    HOTFIX 라는 문구 추가

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -13,7 +13,7 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
-  *This code is meant to test the USB CDC connection!!
+  *HOT FIX -- POLLING METHOD
   ******************************************************************************
   */
 /* USER CODE END Header */
